13b94e76fc4ffb2f62820cf97fa2605c572ad456
ноут <@MPC>
2017-09-11 17:15:06 +0500

    кнопка

--- DonorII/Form1.cs
+++ DonorII/Form1.cs
@@ -16,5 +16,10 @@ namespace DonorII
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("dfgsfghfg");
+        }
     }
 }
