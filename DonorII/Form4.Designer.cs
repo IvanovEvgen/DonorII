36593261bb26d425c497981bf9b78cbd4592301e
SvetlanaZaripova <s.v.e.t.l.a.na@mail.ru>
2017-09-12 18:59:06 +0500

    немного изменений ^^

--- DonorII/Form4.Designer.cs
+++ DonorII/Form4.Designer.cs
@@ -81,9 +81,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(17, 19);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.button1.Location = new System.Drawing.Point(16, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(78, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Назад";
             this.button1.UseVisualStyleBackColor = true;
