1f64d2ad5c771b28aede8f824c59c0e21b271ce4
SvetlanaZaripova <s.v.e.t.l.a.na@mail.ru>
2017-09-25 14:40:45 +0500

    14. Результаты прошедших сдач крови

---
+++ DonorII/Form8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonorII
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
