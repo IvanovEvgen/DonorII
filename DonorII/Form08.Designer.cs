b596d5daee0c2b27fbe99ffa8d4b9cb633645b21
Filchukova <filchukovai@yandex.ru>
2017-10-16 17:36:46 +0500

    1

--- DonorII/Form08.Designer.cs
+++ DonorII/Form08.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -52,26 +53,36 @@
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.database1DataSet = new DonorII.Database1DataSet();
+            this.polBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.polTableAdapter = new DonorII.Database1DataSetTableAdapters.PolTableAdapter();
+            this.bloodTypeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bloodTypeTableAdapter = new DonorII.Database1DataSetTableAdapters.BloodTypeTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bloodTypeBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -80,6 +91,7 @@
             this.Column4});
             this.dataGridView1.Location = new System.Drawing.Point(18, 205);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(564, 143);
             this.dataGridView1.TabIndex = 57;
             // 
@@ -87,6 +99,7 @@
             // 
             this.Column1.HeaderText = "#";
             this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
             this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Column1.Width = 50;
             // 
@@ -94,18 +107,21 @@
             // 
             this.Column2.HeaderText = "Сколько крови сдал (мл)";
             this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             this.Column2.Width = 120;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "ФИО";
             this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
             this.Column3.Width = 250;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Группа крови ";
             this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
             // 
             // label12
             // 
@@ -171,31 +187,39 @@
             // 
             this.comboBox4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "18-21",
+            "22-28",
+            "29-36",
+            "37-.."});
             this.comboBox4.Location = new System.Drawing.Point(412, 149);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(82, 28);
             this.comboBox4.TabIndex = 49;
-            this.comboBox4.Text = "18-29";
             // 
             // comboBox3
             // 
+            this.comboBox3.DataSource = this.polBindingSource;
+            this.comboBox3.DisplayMember = "Gender";
             this.comboBox3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Location = new System.Drawing.Point(412, 114);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(82, 28);
             this.comboBox3.TabIndex = 50;
-            this.comboBox3.Text = "Any";
+            this.comboBox3.ValueMember = "Gender";
             // 
             // comboBox2
             // 
+            this.comboBox2.DataSource = this.bloodTypeBindingSource;
+            this.comboBox2.DisplayMember = "BloodType";
             this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(112, 149);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(203, 28);
             this.comboBox2.TabIndex = 48;
-            this.comboBox2.Text = " I группа";
+            this.comboBox2.ValueMember = "BloodType";
             // 
             // comboBox1
             // 
@@ -241,11 +265,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(10, 117);
+            this.label3.Location = new System.Drawing.Point(60, 117);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.Size = new System.Drawing.Size(45, 20);
             this.label3.TabIndex = 43;
-            this.label3.Text = "ФИО бегуна:";
+            this.label3.Text = "ФИО:";
             // 
             // label2
             // 
@@ -277,17 +301,6 @@
             this.panel1.Size = new System.Drawing.Size(617, 60);
             this.panel1.TabIndex = 39;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(111, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 30);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Донор 2017";
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
@@ -300,6 +313,17 @@
             this.button1.Text = "Назад";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(111, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Донор 2017";
+            // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ScrollBar;
@@ -311,6 +335,30 @@
             this.button3.TabIndex = 58;
             this.button3.Text = "Поиск";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // polBindingSource
+            // 
+            this.polBindingSource.DataMember = "Pol";
+            this.polBindingSource.DataSource = this.database1DataSet;
+            // 
+            // polTableAdapter
+            // 
+            this.polTableAdapter.ClearBeforeFill = true;
+            // 
+            // bloodTypeBindingSource
+            // 
+            this.bloodTypeBindingSource.DataMember = "BloodType";
+            this.bloodTypeBindingSource.DataSource = this.database1DataSet;
+            // 
+            // bloodTypeTableAdapter
+            // 
+            this.bloodTypeTableAdapter.ClearBeforeFill = true;
             // 
             // Form08
             // 
@@ -338,9 +386,13 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form08";
             this.Text = "Результаты прошедших сдач крови ";
+            this.Load += new System.EventHandler(this.Form08_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bloodTypeBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -373,5 +425,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource polBindingSource;
+        private Database1DataSetTableAdapters.PolTableAdapter polTableAdapter;
+        private System.Windows.Forms.BindingSource bloodTypeBindingSource;
+        private Database1DataSetTableAdapters.BloodTypeTableAdapter bloodTypeTableAdapter;
     }
 }
